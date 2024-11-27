70c0fc0ef3ad5b9ac4b009362df5ef23345d1a5d
Alina Alieva <alina.alieva0101@gmail.com>
2024-11-27 13:39:26 +0100

    resolve text message for numbers % 5

--- homework/alina_alieva/task6/task2.py
+++ homework/alina_alieva/task6/task2.py
@@ -1,7 +1,9 @@
 for number in range(1, 101):
     if number % 3 == 0 and number % 5 == 0:
         print('FuzzBuzz')
-    elif number % 3 == 0 or number % 5 == 0:
+    elif number % 3 == 0:
         print('Fuzz')
+    elif number % 5 == 0:
+        print('Buzz')
     else:
         print(number)
