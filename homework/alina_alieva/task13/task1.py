5109d9233dafbe5e7074cc65dd21249f11586859
Alina Alieva <alina.alieva0101@gmail.com>
2025-04-08 11:44:35 +0200

    fixed file path

--- homework/alina_alieva/task13/task1.py
+++ homework/alina_alieva/task13/task1.py
@@ -1,13 +1,15 @@
+import os
 import re
 from datetime import datetime, timedelta
 
 
 def line_action(line):
     data = re.search(r"(\d+).\s+(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\s+-\s+(.*)", line)
+
     if data:
         number = data.group(1)
         day = data.group(2)
-        # action = data.group(3)
+
         date_two_obj = datetime.strptime(day, "%Y-%m-%d %H:%M:%S.%f")
         if number == "1":
             new_date = date_two_obj + timedelta(days=7)
@@ -21,7 +23,10 @@ def line_action(line):
             print(f"3.{difference.days}")
 
 
-with open('/Users/aalieva/Alina/homework/eugene_okulik/hw_13/data.txt', 'r', encoding='utf-8') as file:
+current_dir = os.path.dirname(__file__)
+file_path = os.path.join(current_dir, '..', '..', 'eugene_okulik', 'hw_13', 'data.txt')
+file_path = os.path.normpath(file_path)
+with open(file_path, 'r', encoding='utf-8') as file:
     file = file.readlines()
     for line in file:
         line_action(line)
