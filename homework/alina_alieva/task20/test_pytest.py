ab4e946ccb6403dd9a141f1f30083398fe16a24f
Alina Alieva <alina.alieva0101@gmail.com>
2026-01-07 19:21:22 +0100

    HT 20 Alieva test cases on Pytest fix hometask

--- homework/alina_alieva/task20/test_pytest.py
+++ homework/alina_alieva/task20/test_pytest.py
@@ -18,6 +18,20 @@ def pre_condition_new_object_id():
     requests.delete(f'http://objapi.course.qa-practice.com/object/{object_id}')
 
 
+@pytest.fixture()
+def pre_cond_new_object_id_without_del():
+    body = {"name": "Object Test", "data": {"test": "Body data 1", "desc": "Data 2"}}
+    headers = {'Content-Type': 'application/json'}
+    response_obj = requests.post(
+        'http://objapi.course.qa-practice.com/object',
+        json=body,
+        headers=headers
+    )
+    object_id = response_obj.json()['id']
+    print(object_id)
+    return object_id
+
+
 @pytest.fixture(scope='session', autouse=True)
 def text_title():
     print('Start testing')
@@ -62,58 +76,33 @@ def test_add_new_object(body):
 
 @pytest.mark.medium
 # @pytest.mark.skip('No preconditions')
-def test_edit_all_object():
-    body_create = {"name": "Object for edit", "data": {"test": "Test", "desc": "Desc"}}
-    headers = {'Content-Type': 'application/json'}
-    response_create = requests.post(
-        'http://objapi.course.qa-practice.com/object',
-        json=body_create,
-        headers=headers
-    )
-    object_id = response_create.json()['id']
-
+def test_edit_all_object(pre_condition_new_object_id):
     body = {"name": "Test Name2", "data": {"test": "Update name", "desc": "Trula"}}
     headers = {'Content-Type': 'application/json'}
     response_put = requests.put(
-        f'http://objapi.course.qa-practice.com/object/{object_id}',
+        f'http://objapi.course.qa-practice.com/object/{pre_condition_new_object_id}',
         json=body,
         headers=headers
     ).json()
     assert response_put['name'] == 'Test Name2'
-    requests.delete(f'http://objapi.course.qa-practice.com/object/{object_id}')
+    requests.delete(f'http://objapi.course.qa-practice.com/object/{pre_condition_new_object_id}')
 
 
 # @pytest.mark.skip('No preconditions')
-def test_update_object():
-    body_create = {"name": "Object for patch", "data": {"test": "Test", "desc": "Desc"}}
-    headers = {'Content-Type': 'application/json'}
-    response_create = requests.post(
-        'http://objapi.course.qa-practice.com/object',
-        json=body_create,
-        headers=headers
-    )
-    object_id = response_create.json()['id']
-
+def test_update_object(pre_condition_new_object_id):
     body = {"data": {"test": "random text - 0.1", "desc": "Edit 2025"}}
     headers = {'Content-Type': 'application/json'}
     response_patch = requests.patch(
-        f'http://objapi.course.qa-practice.com/object/{object_id}',
+        f'http://objapi.course.qa-practice.com/object/{pre_condition_new_object_id}',
         json=body,
         headers=headers
     ).json()
     assert response_patch['data']['test'] == "random text - 0.1"
-    requests.delete(f'http://objapi.course.qa-practice.com/object/{object_id}')
+    requests.delete(f'http://objapi.course.qa-practice.com/object/{pre_condition_new_object_id}')
 
 
 # @pytest.mark.skip('No preconditions')
-def test_delete_object():
-    body_create = {"name": "Object for delete", "data": {"test": "Test", "desc": "Desc"}}
-    headers = {'Content-Type': 'application/json'}
-    response_create = requests.post(
-        'http://objapi.course.qa-practice.com/object',
-        json=body_create,
-        headers=headers
-    )
-    object_id = response_create.json()['id']
-    response_delete = requests.delete(f'http://objapi.course.qa-practice.com/object/{object_id}')
-    assert response_delete.status_code == 200
+def test_delete_object(pre_cond_new_object_id_without_del):
+    object_id = pre_cond_new_object_id_without_del
+    response = requests.delete(f'http://objapi.course.qa-practice.com/object/{object_id}')
+    assert response.status_code == 200
